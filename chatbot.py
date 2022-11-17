6abfad576dc398eb47f303b1889b264f83955bae
GeovanaF6 <geovana.f16@aluno.ifsc.edu.br>
2022-11-17 18:04:09 +0000

    Mudanças de código

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,7 @@ from re import fullmatch
 from os import getenv
 from os.path import exists
 from dotenv import load_dotenv
+
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -34,69 +35,62 @@ async def on_message(msg):
     else:
         return
     #
-    # Testar se o canal é pvt (msg.channel.type.name == 'private')
-    # e, se for, avisar o jogador
-    if msg.channel.type.name == 'private':
-        await msg.channel.send(frases['canal_privado'])
-        return
-    #
     # Garantir que o autor tem dados de partida
     if autor not in partidas:
         #
         # Jogador começa no estado 0 com duas chaves
         partidas[autor] = {
-            'estado': 0,
-            'inventario': set()
+            'estado': 0
         }
     #
-    # Testar se o jogador está em canal de voz,
-    # caso contrário convidá-lo a entrar em um
-    if msg.author.voice:
-        if msg.guild.me not in msg.author.voice.channel.members:
-            partidas[autor]['canal_de_voz'] = await msg.author.voice.channel.connect()
-        canal_de_voz = partidas[autor]['canal_de_voz']
-    else:
-        await msg.channel.send(frases['sem_canal_de_voz'])
-        return
+    # Testar se o canal é pvt (msg.channel.type.name == 'private')
+    # e, se for, avisar o jogador e continua o jogo sem áudio
+    if msg.channel.type.name == 'private':
+        # Avisar ao jogador apenas quando o estado for 0
+        if(partidas[autor]['estado'] == 0):
+            await msg.channel.send(frases['canal_privado'])
+            await msg.channel.send(frases['sem_canal_de_voz'])
+            partidas[autor]['canal_de_voz'] = None
+    #
+    # Testar se a mensagem foi mandada em um chat de servidor
+    # se sim, testar se o jogador está em canal de voz,
+    # caso não esteja convidá-lo a entrar em um.
+    if msg.channel.type.name != 'private':
+        if msg.author.voice:
+            if msg.guild.me not in msg.author.voice.channel.members:
+                partidas[autor]['canal_de_voz'] = await msg.author.voice.channel.connect()
+            canal_de_voz = partidas[autor]['canal_de_voz']
+        else:
+            await msg.channel.send(frases['sem_canal_de_voz'])
+            return
     #
     # Criar variáveis locais para melhorar legibilidade do código
     estado_do_jogador = estados[partidas[autor]['estado']]
-    inventario_do_jogador = partidas[autor]['inventario']
     #
     # Varrer os possíveis próximos estados para validar com a mensagem do usuário
     for key, value in estado_do_jogador['proximos_estados'].items():
         if fullmatch(key, mensagem):
             #
-            # Verificar se o jogador possui inventário mínimo para avançar
-            if inventario_do_jogador.issuperset(estados[value]['inventario']):
-                #
-                # Atualiza o estado do jogador
-                partidas[autor]['estado'] = value
-                #
-                # Remove os itens de inventário requisitados
-                partidas[autor]['inventario'] = inventario_do_jogador.difference(
-                    estados[value]['inventario'])
-                #
-                # Se houver um som referente ao estado,
-                # toca no canal de voz do jogador
+            # Atualiza o estado do jogador
+            partidas[autor]['estado'] = value
+            #
+            # Se houver um som referente ao estado,
+            # toca no canal de voz do jogador
+            if msg.channel.type.name != 'private':
                 arquivo_de_som = str(value) + '.mp3'
                 if exists(arquivo_de_som):
                     #
                     # Conectar no canal de áudio e emitir o som
                     som_opus = await discord.FFmpegOpusAudio.from_probe(arquivo_de_som)
                     canal_de_voz.play(som_opus)
-                #
-                # Se houver uma imagem referente ao estado, enviar
-                arquivo_de_imagem = str(value) + '.png'
-                if exists(arquivo_de_imagem):
-                    await msg.channel.send(file=discord.File(arquivo_de_imagem))
-                #
-                # Criar uma lista de frases usando o delimitador '|' e enviar uma a uma
-                [await msg.channel.send(i) for i in choice(estados[value]['frases']).split('|')]
-            else:
-                #
-                # Retornar mensagem (e manter jogador no atual estado)
-                await msg.channel.send(frases['inventario_insuficiente'])
+            #
+            # Se houver uma imagem referente ao estado, enviar
+            arquivo_de_imagem = str(value) + '.png'
+            if exists(arquivo_de_imagem):
+                await msg.channel.send(file=discord.File(arquivo_de_imagem))
+            #
+            # Criar uma lista de frases usando o delimitador '|' e enviar uma a uma
+            [await msg.channel.send(i) for i in choice(estados[value]['frases']).split('|')]
             return
     #
     # Sempre responder ao usuário (dica ou não)
